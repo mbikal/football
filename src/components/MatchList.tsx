672497c1b16f2c4107d53ad4ce8e0c9a86f7de29
mab191207-stack <mab191207@gmail.com>
2026-05-30 17:23:09 +0545

    ucl final

--- src/components/MatchList.tsx
+++ src/components/MatchList.tsx
@@ -1,12 +1,6 @@
 import MatchContainer from "./MatchContainer";
-import liverpoolLogo from "../assets/liverpool.png";
-import arsenalLogo from "../assets/arsenal.png";
-import brightonLogo from "../assets/brighton.png";
-import brentfordLogo from "../assets/brentford.png";
-import cityLogo from "../assets/city.png";
-import astonvilla from "../assets/astonvilla.png";
-import unitedLogo from "../assets/united.png";
-import palaceLogo from "../assets/palace.png";
+import brightonLogo from "../assets/psg.png";
+import unitedLogo from "../assets/arsenal.png";
 
 function MatchList() {
   return (
@@ -18,35 +12,8 @@ function MatchList() {
         iframeUrl="https://securedq.blogspot.com/p/6.html"
         team1img={brightonLogo}
         team2img={unitedLogo}
-        team1name="Brighton"
-        team2name="Manchester United"
-      />
-
-      <MatchContainer
-        matchDate="2026-05-24T20:45:00+05:45"
-        iframeUrl="https://securedq.blogspot.com/p/2.html"
-        team1img={palaceLogo}
-        team2img={arsenalLogo}
-        team1name="Crystal Palace"
+        team1name="PSG"
         team2name="Arsenal"
-      />
-
-      <MatchContainer
-        matchDate="2026-05-24T20:45:00+05:45"
-        iframeUrl="https://securedq.blogspot.com/p/17.html"
-        team1img={liverpoolLogo}
-        team2img={brentfordLogo}
-        team1name="Liverpool"
-        team2name="Brentford"
-      />
-
-      <MatchContainer
-        matchDate="2026-05-24T20:45:00+05:45"
-        iframeUrl="https://securedq.blogspot.com/p/7.html"
-        team1img={cityLogo}
-        team2img={astonvilla}
-        team1name="Man City"
-        team2name="Aston Villa"
       />
     </div>
   );
